172d38a9bac111a659f9a97d35f19504e9d53839
Letlhogonolo Rakgantsho <107153213+Eaziey@users.noreply.github.com>
2025-09-24 21:02:05 +0000

    cursor pointer on logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,10 +18,6 @@ const Navbar = ({scrollToSection , refs}) =>{
   const handleLinkClick = (e, name) =>{
     e.preventDefault();
 
-    //console.log(name);
-    //console.log(refs);
-
-
     const sectionRef = refs[name];
 
     if(!sectionRef?.current) return;
@@ -59,32 +55,6 @@ const Navbar = ({scrollToSection , refs}) =>{
     setIsMobileMenuOpen(!isMobileMenuOpen);
   }
 
-  //for smooth scroll
-  /*const handleLinkClick = (e, href) => {
-    e.preventDefault;
-
-    const targetElement = document.querySelector(href);
-
-    if(targetElement){
-      const offset = -85; //try removing this and see what happens//
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.scrollY + offset;
-
-
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-      })
-    }
-
-    setIsMobileMenuOpen(false);
-
-
-  }*/
-
-
-
-
   return(
       <motion.nav 
         className='fixed left-0 right-0 top-4 z-50'
@@ -104,8 +74,8 @@ const Navbar = ({scrollToSection , refs}) =>{
           }
         }}
         >
-          <div onClick={(e) => handleLinkClick(e, "hero")} className='flex items-center justify-center gap-6 rounded-full border-black border-2 mx-10 bg-white w-72' >
-            <a >
+          <div onClick={(e) => handleLinkClick(e, "hero")} className='flex items-center justify-center gap-6 rounded-full border-black border-2 mx-10 bg-white w-72 cursor-pointer' >
+            <a className="" >
               <img src = {logo} width={60} height={60} alt= "logo" className=" mx-10" />
             </a>
           </div>
@@ -118,7 +88,7 @@ const Navbar = ({scrollToSection , refs}) =>{
         {/*mobile menu */}
         <div className='mx-2 items-center justify-center rounded-full border-2 backdrop-blur-md lg:hidden p-1'>
           <div className='flex items-center justify-between'>
-            <div className='flex items-center justify-center gap-6 rounded-full border-black border-2 mx-2 bg-white w-auto'>
+            <div className='flex items-center justify-center gap-6 rounded-full border-black border-2 mx-2 bg-white w-auto cursor-pointer'>
               <a href="#">
                 <img src = {logo} alt='logo' width={40} height={40} className=" mx-4"/>
               </a>
@@ -146,62 +116,6 @@ const Navbar = ({scrollToSection , refs}) =>{
   )
 }
 
-/*function Navbar() {
-  return (
-    <Disclosure as="nav" className="">
-      <div className="mx-10 max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            
-            {/* Mobile menu button}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
-            </DisclosureButton>
-          </div>
-
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
-              <img
-                alt="MyLogo"
-                src=""
-                className="h-8 w-auto"
-              />
-            </div>
-
-            <div className="hidden sm:ml-6 sm:block">
-              
-                <SlideTabs nav = {navigation} />
-              
-            </div>
-          </div>
-          
-        </div>
-      </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
-  )
-}*/
 
 
 export default Navbar;
